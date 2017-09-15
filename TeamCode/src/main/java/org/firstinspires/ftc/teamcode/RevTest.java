d3ace14db0526d8201bce7db6bbaf2385c42b85e
darrenlee01 <darrenjwlee01@gmail.com>
2017-09-15 16:28:15 -0700

    untested imutest program added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RevTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RevTest.java
@@ -11,9 +11,12 @@ import com.qualcomm.robotcore.hardware.I2cAddr;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 
 @com.qualcomm.robotcore.eventloop.opmode.Autonomous(name = "RevTest")
+
+
 public class RevTest extends LinearOpMode {
     static DcMotor motor1;
     DriveTrain driveTrain;
@@ -22,21 +25,24 @@ public class RevTest extends LinearOpMode {
     static DcMotor rF, rB, lF, lB;
     static GyroSensor gyro;
 
+
     static Servo servo1;
     //static GyroSensor gyro;
     @Override
     public void runOpMode() throws InterruptedException {
-
+        Orientation angles;
         initHardware();
         waitForStart();
         while (opModeIsActive()) {
-            //driveTrain.moveBkwd(0.2,5,10);
-            //Functions.waitFor(10000);
-            //driveTrain.moveFwd(0.2,5,10);
-            //Functions.waitFor(10000000);
+
+            driveTrain.moveBkwd(0.2,5,10);
+            Functions.waitFor(10000);
+            driveTrain.moveFwd(0.2,5,10);
+            Functions.waitFor(10000000);
 
             driveTrain.rotateGyroRamp(45, 0.2, 15, gyro, telemetry);
             Functions.waitFor(10000);
+
 
             //driveTrain.moveFwd(0.2,5,10);
             //driveTrain.moveFwd();
@@ -55,6 +61,8 @@ public class RevTest extends LinearOpMode {
     public void initHardware() {
         gyro = hardwareMap.gyroSensor.get("gyro");
         floorColor = hardwareMap.colorSensor.get("floorColor");
+
+
         rF = hardwareMap.dcMotor.get("rF");
         rB = hardwareMap.dcMotor.get("rB");
         lF = hardwareMap.dcMotor.get("lF");
