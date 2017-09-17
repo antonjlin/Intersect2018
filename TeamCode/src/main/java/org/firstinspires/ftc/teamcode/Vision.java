c7a8250ffeef2b21aaf9420169ef5df89efcaa37
Pranav Garg <garg.pranav@gmail.com>
2017-09-17 16:28:15 -0700

    Library loaded correctly

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.VisibleForTesting;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,13 +23,22 @@ import com.qualcomm.robotcore.hardware.DeviceManager;
 import org.firstinspires.ftc.robotcontroller.internal.FtcOpModeRegister;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.lasarobotics.vision.android.Cameras;
+import org.lasarobotics.vision.android.Sensors;
 import org.lasarobotics.vision.detection.ColorBlobDetector;
 import org.lasarobotics.vision.ftc.resq.Beacon;
 import org.lasarobotics.vision.opmode.ManualVisionOpMode;
+import org.lasarobotics.vision.opmode.VisionOpMode;
+//import org.lasarobotics.vision.opmode.VisionOpModeCore;
+import org.lasarobotics.vision.util.FPS;
 import org.lasarobotics.vision.util.ScreenOrientation;
 import org.lasarobotics.vision.util.color.Color;
 import org.lasarobotics.vision.util.color.ColorHSV;
 import org.lasarobotics.vision.util.color.ColorSpace;
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfByte;
@@ -52,6 +62,7 @@ import java.util.concurrent.TimeUnit;
 
 import static android.R.id.input;
 import static android.R.id.keyboardView;
+import static com.sun.tools.javac.util.Assert.error;
 import static org.lasarobotics.vision.opmode.VisionOpMode.rotation;
 import static org.opencv.imgproc.Imgproc.CHAIN_APPROX_SIMPLE;
 import static org.opencv.imgproc.Imgproc.CV_CHAIN_APPROX_SIMPLE;
@@ -75,16 +86,71 @@ import static org.opencv.imgproc.Imgproc.*;
 
 
 public class Vision extends OpMode {
+    private static final int initialMaxSize = 1200;
+    public static JavaCameraView openCVCamera;
+    private static boolean initialized = false;
+    private static boolean openCVInitialized = false;
+    public int width, height;
+    public FPS fps;
+    public Sensors sensors;
 
     Runnable run = new Runnable() {
         //@Override
         public void run() {
         }
     };
-    VideoCapture capture = new VideoCapture(0);
+
+
    // ImageView view  = (ImageView) View.findViewById(R.id.MyImageDisplay);
 
     public void init(){
+        BaseLoaderCallback openCVLoaderCallback = null;
+        try {
+            openCVLoaderCallback = new BaseLoaderCallback(hardwareMap.appContext) {
+                @Override
+                public void onManagerConnected(int status) {
+                    switch (status) {
+                        case LoaderCallbackInterface.SUCCESS: {
+                            //Woohoo!
+                            Log.d("OpenCV", "OpenCV Manager connected!");
+                            openCVInitialized = true;
+                        }
+                        break;
+                        default: {
+                            super.onManagerConnected(status);
+                        }
+                        break;
+                    }
+                }
+            };
+        } catch (NullPointerException e) {
+            error("Could not find OpenCV Manager!\r\n" +
+                    "Please install the app from the Google Play Store.");
+        }
+
+        final Activity
+                activity = (Activity) hardwareMap.appContext;
+        final Vision t = this;
+
+        if (!OpenCVLoader.initDebug()) {
+            Log.d("OpenCV", "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+            boolean success = OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, hardwareMap.appContext, openCVLoaderCallback);
+            if (!success) {
+                Log.e("OpenCV", "Asynchronous initialization failed!");
+                error("Could not initialize OpenCV!\r\n" +
+                        "Did you install the OpenCV Manager from the Play Store?");
+            } else {
+                Log.d("OpenCV", "Asynchronous initialization succeeded!");
+            }
+        } else {
+            Log.d("OpenCV", "OpenCV library found inside package. Using it!");
+            if (openCVLoaderCallback != null)
+                openCVLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+            else {
+                Log.e("OpenCV", "Failed to load OpenCV from package!");
+                return;
+            }
+        }
 
 
     }
@@ -92,9 +158,12 @@ public class Vision extends OpMode {
 
 
     public void loop() {
+        VideoCapture capture = new VideoCapture(0);
         Mat frame = new Mat();
         Mat greyframe = new Mat();
         capture.read(frame);
+        Imgproc.cvtColor(frame, greyframe, Imgproc.COLOR_RGB2GRAY);
+        //JavaCameraView camera;
         int id = hardwareMap.appContext.getResources().getIdentifier("MyVision", "id", hardwareMap.appContext.getPackageName());
         View view = ((Activity) hardwareMap.appContext).findViewById(id);
         ImageView view1 = (ImageView) view;
