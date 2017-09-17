b66448642fda7aa0a442271c5aa4b4b4d7266cd0
darrenlee01 <darrenjwlee01@gmail.com>
2017-09-17 15:53:36 -0700

    added tests such as encoder, imu.getAngle(), and imu turning function for DriveTrainTesting class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrainTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrainTesting.java
@@ -10,57 +10,84 @@ import com.qualcomm.robotcore.hardware.GyroSensor;
 import com.qualcomm.robotcore.hardware.I2cAddr;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.robotcore.external.Func;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 
 @com.qualcomm.robotcore.eventloop.opmode.Autonomous(name = "RevTest")
 
 
-public class RevTest extends LinearOpMode {
+public class DriveTrainTesting extends LinearOpMode {
     static DcMotor motor1;
     DriveTrain driveTrain;
     static ColorSensor floorColor;
 
     static DcMotor rF, rB, lF, lB;
     static GyroSensor gyro;
-
+    private BNO055IMU adaImu;
+    private IMU imu;
 
     static Servo servo1;
     //static GyroSensor gyro;
     @Override
     public void runOpMode() throws InterruptedException {
-        Orientation angles;
         initHardware();
         waitForStart();
         while (opModeIsActive()) {
 
+            //    TEST 1
+            //test encoder functions once drive train is set up
             driveTrain.moveBkwd(0.2,5,10);
             Functions.waitFor(10000);
             driveTrain.moveFwd(0.2,5,10);
             Functions.waitFor(10000000);
 
-            driveTrain.rotateGyroRamp(180, 0.2, 15, gyro, telemetry);
-            Functions.waitFor(10000);
-            driveTrain.rotateGyroRamp(-180, 0.2, 15, gyro, telemetry);
+            //    TEST 2
+            //test imu.getAngle function by turning robot a few times
+            //Turn right by around 90 degrees
+            telemetry.addData("Heading", imu.getAngle());
+            telemetry.update();
+            Functions.waitFor(5000);
+            //turn left by around 90 degrees
+            telemetry.addData("Heading", imu.getAngle());
+            telemetry.update();
+            Functions.waitFor(5000);
+            //turn left by around 90 degrees again
+            telemetry.addData("Heading", imu.getAngle());
+            telemetry.update();
+            Functions.waitFor(5000);
+            //turn left by around 180 degrees
+            telemetry.addData("Heading", imu.getAngle());
+            telemetry.update();
 
 
+            Functions.waitFor(100000000);
+            //    TEST 3
+            //test imu turning
+            driveTrain.rotateIMURamp(90, 0.2, 10, telemetry);
+            Functions.waitFor(5000);
+            driveTrain.rotateIMURamp(-90, 0.2, 10, telemetry);
+            Functions.waitFor(5000);
+            driveTrain.rotateIMURamp(-90, 0.2, 10, telemetry);
 
-            //driveTrain.moveFwd(0.2,5,10);
-            //driveTrain.moveFwd();
-            //driveTrain.moveFwd(0.2,10,10);
-            //driveTrain.moveBkwRight(0.2, 0.2, 5, 10);
-
-
-
-
-
-
+            Functions.waitFor(100000000);
 
         }
     }
 
     public void initHardware() {
+
+        adaImu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu = new IMU(adaImu);
+
         gyro = hardwareMap.gyroSensor.get("gyro");
         floorColor = hardwareMap.colorSensor.get("floorColor");
 
