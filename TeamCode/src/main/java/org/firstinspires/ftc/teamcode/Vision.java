9128482edac0c3ec8c002af8292127c2393af59d
Pranav Garg <garg.pranav@gmail.com>
2017-09-17 14:19:22 -0700

    started adding video to robot controller

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision.java
@@ -1,7 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
+import android.app.Activity;
+import android.graphics.Bitmap;
 import android.media.Image;
 import android.os.Bundle;
+import android.support.annotation.VisibleForTesting;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,11 +15,11 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 
-
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.*;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DeviceManager;
 
+import org.firstinspires.ftc.robotcontroller.internal.FtcOpModeRegister;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.lasarobotics.vision.android.Cameras;
 import org.lasarobotics.vision.detection.ColorBlobDetector;
@@ -26,6 +29,7 @@ import org.lasarobotics.vision.util.ScreenOrientation;
 import org.lasarobotics.vision.util.color.Color;
 import org.lasarobotics.vision.util.color.ColorHSV;
 import org.lasarobotics.vision.util.color.ColorSpace;
+import org.opencv.android.Utils;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfByte;
 import org.opencv.core.MatOfPoint;
@@ -47,6 +51,7 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 import static android.R.id.input;
+import static android.R.id.keyboardView;
 import static org.lasarobotics.vision.opmode.VisionOpMode.rotation;
 import static org.opencv.imgproc.Imgproc.CHAIN_APPROX_SIMPLE;
 import static org.opencv.imgproc.Imgproc.CV_CHAIN_APPROX_SIMPLE;
@@ -61,6 +66,14 @@ import static org.opencv.imgproc.Imgproc.*;
  * Created by pranav on 9/15/17.
  */
 
+//@SuppressWarnings("ALL")
+
+@com.qualcomm.robotcore.eventloop.opmode.Autonomous(name = "Vision1")
+
+//@com.qualcomm.robotcore.eventloop.opmode.OpModeRegistrar("vision", Vision)
+//@com.qualcomm.robotcore.eventloop.opmode.TeleOp\
+
+
 public class Vision extends OpMode {
 
     Runnable run = new Runnable() {
@@ -68,40 +81,46 @@ public class Vision extends OpMode {
         public void run() {
         }
     };
-
-    ImageView view  = (ImageView) View.findViewById(R.id.MyImageDisplay);
+    VideoCapture capture = new VideoCapture(0);
+   // ImageView view  = (ImageView) View.findViewById(R.id.MyImageDisplay);
 
     public void init(){
 
-        ImageView view = new ImageView();
+
     }
 
 
 
     public void loop() {
-
-        VideoCapture capture = new VideoCapture(0);
         Mat frame = new Mat();
         Mat greyframe = new Mat();
         capture.read(frame);
-        telemetry.addData();
+        int id = hardwareMap.appContext.getResources().getIdentifier("MyVision", "id", hardwareMap.appContext.getPackageName());
+        View view = ((Activity) hardwareMap.appContext).findViewById(id);
+        ImageView view1 = (ImageView) view;
+        Bitmap bm = Bitmap.createBitmap(greyframe.cols(), greyframe.rows(), Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(greyframe , bm);
+        view1.setImageBitmap(bm);
+
+
+
+
+
+
+
+
+
+
 
 
         List<MatOfPoint> countours = new ArrayList<MatOfPoint>(){};
         Mat Hierarchy = new Mat();
-
         findContours(greyframe,countours,Hierarchy,RETR_CCOMP, CHAIN_APPROX_SIMPLE);
-
         //drawContours();
-
-
-
-
-
         /*ColorHSV min = new ColorHSV(255,50,80);
         ColorHSV max = new ColorHSV(205,100,100);
         ColorBlobDetector detector = new ColorBlobDetector(min, max);
-        System.out.print(detector.getContours());
+        System.out.print(detector.getContours(
         telemetry.addData("getCountours",detector.getContours());*/
         Imgproc.cvtColor(frame, greyframe, Imgproc.COLOR_RGB2GRAY);
 
@@ -112,8 +131,6 @@ public class Vision extends OpMode {
 
 
     }
-    public void runOpMode(){
 
-    }
 
 }
