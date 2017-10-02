59c098d3ad6f7177b0746e4de564d93c6572eeb7
antonjlin <antonjlin@gmail.com>
2017-10-01 20:53:48 -0700

    added xyz orientation function

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IMU.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IMU.java
@@ -8,6 +8,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.Quaternion;
 
 /**
  * Created by Howard on 11/9/16.
@@ -37,6 +38,23 @@ public class IMU {
         }
         return angle;
     }
+
+    public double[] getOrientation() {
+        Quaternion quatAngles = adafruit.getQuaternionOrientation();
+
+        double w = quatAngles.w;
+        double x = quatAngles.x;
+        double y = quatAngles.y;
+        double z = quatAngles.z;
+
+        // for the Adafruit IMU, yaw and roll are switched
+        double roll = Math.atan2( 2*(w*x + y*z) , 1 - 2*(x*x + y*y) ) * 180.0 / Math.PI;
+        double pitch = Math.asin( 2*(w*y - x*z) ) * 180.0 / Math.PI;
+        double yaw = Math.atan2( 2*(w*z + x*y), 1 - 2*(y*y + z*z) ) * 180.0 / Math.PI;
+
+        return new double[]{yaw, pitch, roll};
+    }
+
 
     public double getRadians(){
         return getAngle()*Math.PI/180;
