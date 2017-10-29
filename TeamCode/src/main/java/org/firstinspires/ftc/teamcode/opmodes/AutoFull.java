5a00887001461712ae2559c2c371da0378f9db0c
Pranav Garg <garg.pranav@gmail.com>
2017-10-28 19:58:05 -0700

    auto code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutoFull.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutoFull.java
@@ -9,24 +9,31 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.GyroSensor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.robotutil.DriveTrain;
 import org.firstinspires.ftc.teamcode.robotutil.Functions;
 import org.firstinspires.ftc.teamcode.robotutil.IMU;
 import org.firstinspires.ftc.teamcode.robotutil.VisionProcessing;
 
+import static org.firstinspires.ftc.teamcode.robotutil.DriveTrain.Direction.*;
+
 @Autonomous(name = "AutoFull")
 public class AutoFull extends LinearOpMode {
     static  DcMotor rF, rB, lF, lB, flywheel1, flywheel2, sweeperLow;
+    static Servo rServo;
     static GyroSensor gyro;
     static ColorSensor jewelColor;
     boolean red = false;
-    DriveTrain driveTrain;
+    boolean redBallIsLeft;
     char alliance;
     int state;
     private BNO055IMU adaImu;
     private IMU imu;
     public int crypHeading = 0;
+
+    DriveTrain driveTrain = new DriveTrain(this);
+
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -34,7 +41,15 @@ public class AutoFull extends LinearOpMode {
         initHardware();
         waitForStart();
         state = 0;// Todo:
-        if (opModeIsActive()) {
+        if(red) {
+            if (redBallIsLeft) driveTrain.encoderDrive(.3, 4, BACKWARD, 5000);
+            else driveTrain.encoderDrive(.3, 4, FORWARD, 5000);
+
+            driveTrain.encoderDrive(.3, 12, FORWARD,5000); //until wall detected
+
+            driveTrain.encoderDrive(.3, 4,LEFT,5000); //until
+            //detect cryptobox/align
+
 
         }
     }
@@ -46,47 +61,49 @@ public class AutoFull extends LinearOpMode {
         rB = hardwareMap.dcMotor.get("rB");
         lF = hardwareMap.dcMotor.get("lF");
         lB = hardwareMap.dcMotor.get("lB");
+        rServo = hardwareMap.servo.get("rServo");
         lB.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         lF.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rB.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rF.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        lF.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rF.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rB.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        lB.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rF.setDirection(DcMotor.Direction.FORWARD);
-        rB.setDirection(DcMotor.Direction.FORWARD);
-        lB.setDirection(DcMotor.Direction.REVERSE);
-        lF.setDirection(DcMotor.Direction.REVERSE);
-        adaImu = hardwareMap.get(BNO055IMU.class, "imu");
-        imu = new IMU(adaImu);
-        jewelColor = hardwareMap.colorSensor.get("jewelColor");
-        sweeperLow = hardwareMap.dcMotor.get("sweeperLow");
-        crypHeading = (int) imu.getAngle() - 90;
+            lF.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            rF.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            rB.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            lB.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            rF.setDirection(DcMotor.Direction.FORWARD);
+            rB.setDirection(DcMotor.Direction.FORWARD);
+            lB.setDirection(DcMotor.Direction.REVERSE);
+            lF.setDirection(DcMotor.Direction.REVERSE);
+            adaImu = hardwareMap.get(BNO055IMU.class, "imu");
+            imu = new IMU(adaImu);
+            jewelColor = hardwareMap.colorSensor.get("jewelColor");
+            sweeperLow = hardwareMap.dcMotor.get("sweeperLow");
+            crypHeading = (int) imu.getAngle() - 90;
 
-        gyro = hardwareMap.gyroSensor.get("gyro");
-        driveTrain = new DriveTrain(this);
-        driveTrain.detectAmbientLight(jewelColor);
+            gyro = hardwareMap.gyroSensor.get("gyro");
+            driveTrain = new DriveTrain(this);
+            driveTrain.detectAmbientLight(jewelColor);
 
-        //driveTrain.calibrateGyro(telemetry);
 
-    }
+            //driveTrain.calibrateGyro(telemetry);
 
-    public void options(){
-        telemetry.addData("Team", "Blue");
-        telemetry.update();
-        boolean confirmed = false;
-        red = false;
-        while(!confirmed){
-            if (gamepad1.a){
-                red = true;
-                telemetry.addData("Team", red ? "Red": "Blue");
-            }
-            if (gamepad1.b){
-                red = false;
-                telemetry.addData("Team", red ? "Red": "Blue");
-            }
+        }
+
+        public void options(){
+            telemetry.addData("Team", "Blue");
             telemetry.update();
+            boolean confirmed = false;
+            red = false;
+            while(!confirmed){
+                if (gamepad1.a){
+                    red = true;
+                    telemetry.addData("Team", red ? "Red": "Blue");
+                }
+                if (gamepad1.b){
+                    red = false;
+                    telemetry.addData("Team", red ? "Red": "Blue");
+                }
+                telemetry.update();
 
             if (gamepad1.left_stick_button && gamepad1.right_stick_button){
                 telemetry.addData("Team", red ? "Red" : "Blue");
