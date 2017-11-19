c5d4374802c4e62e597c0b534842e339988e10bd
animaddhuri <anirudh.maddhuri.gold@gmail.com>
2017-11-18 23:53:26 -0800

    Merge branch 'alpha' of https://github.com/theintersect/Intersect2018
    # Conflicts:
    #	FtcRobotController/build.gradle
    #	TeamCode/build.gradle
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutoFull.java
    #	gradle/wrapper/gradle-wrapper.properties
    #	opencv-java/build.gradle

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/ColorSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/ColorSensorTest.java
@@ -2,13 +2,17 @@
 package org.firstinspires.ftc.teamcode.tests;
 
 import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsI2cColorSensor;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.GyroSensor;
+import com.qualcomm.robotcore.hardware.HardwareDevice;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import org.firstinspires.ftc.robotcore.external.Func;
 import org.firstinspires.ftc.teamcode.robotutil.DriveTrain;
 import org.firstinspires.ftc.teamcode.robotutil.Functions;
 import org.firstinspires.ftc.teamcode.robotutil.IMU;
@@ -17,22 +21,37 @@ import org.firstinspires.ftc.teamcode.robotutil.Team;
 
 @Autonomous(name = "Color Sensor Test")
 public class ColorSensorTest extends LinearOpMode {
-
+    // ModernRoboticsI2cColorSensor jewelColor;
     ColorSensor jewelColor;
-    MRColorSensor colorSensor;
+    //MRColorSensor colorSensor;
 
     @Override
     public void runOpMode() throws InterruptedException {
-        initHardware();
+        //initHardware();
+        jewelColor = hardwareMap.colorSensor.get("jewelColor");
+        jewelColor.enableLed(false);
+        Functions.waitFor(100);
+        jewelColor.enableLed(true);
+        Functions.waitFor(100);
+        jewelColor.enableLed(false);
+        Functions.waitFor(100);
+        jewelColor.enableLed(true);
+        Functions.waitFor(100);
+        jewelColor.enableLed(false);
+        Functions.waitFor(100);
+        jewelColor.enableLed(true);
+        Functions.waitFor(100);
+        jewelColor.enableLed(false);
+        Functions.waitFor(100);
+        jewelColor.enableLed(true);
+        Functions.waitFor(100);
         waitForStart();
-        while(opModeIsActive())
-            colorSensor.telemetryDebug(this);
-    }
-
-    public void initHardware() {
-        colorSensor = new MRColorSensor(jewelColor, this);
-        colorSensor.enableLED(false);
-        telemetry.addLine("Done");
-        telemetry.update();
+        while (opModeIsActive()) {
+            //colorSensor.telemetryDebug(this);
+            telemetry.addData("blue: ", jewelColor.blue());
+            telemetry.addData("red: ", jewelColor.red());
+            //colorSensor = new MRColorSensor(jewelColor, this);
+            telemetry.update();
+        }
     }
 }
