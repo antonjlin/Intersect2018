0402381974d0184ce43e603d62a1faa799ac87a3
animaddhuri <anirudh.maddhuri.gold@gmail.com>
2017-12-16 19:43:09 -0800

    Merge branch 'alpha' of https://github.com/theintersect/Intersect2018
    # Conflicts:
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutoFull.java
    #	opencv-java/build.gradleF
    
    mid saratoga  - updated auton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Outreach.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Outreach.java
@@ -23,7 +23,7 @@ public class Outreach extends LinearOpMode {
     DriveTrain driveTrain;
     static GyroSensor gyro;
     static ColorSensor floorColor;
-    static DcMotor rIntake, lIntake, lSlide, rSlide;
+    static DcMotor rIntake, lIntake, lSlide, rSlide,lWheel,rWheel;
     private BNO055IMU adaImu;
     private IMU imu;
     int lSlidePos;
@@ -43,58 +43,29 @@ public class Outreach extends LinearOpMode {
         waitForStart();
 
         while (opModeIsActive()) {
-            double leftPower = scaleDrive(-gamepad1.left_stick_y);
-            double rightPower = scaleDrive(-gamepad1.right_stick_y);
+            double leftPower = scaleDrive(gamepad1.left_stick_y);
+            double rightPower = scaleDrive(gamepad1.right_stick_y);
             rightSide.setPower(rightPower);
             leftSide.setPower(leftPower);
         }
     }
 
     private double scaleDrive(double x) {
-        if (x < .1 && x > -.1) {
-            // Game pad has a little drift. At resting, it may return +/-0,05, set a dead zone here.
-            return 0.0;
-        }
+
         // limit to +/- 1.0 motor power
-        if (x > 1) {
+        if (x > .1) {
             return (1);
         }
-        if (x < -1) {
+        if (x < -.1) {
             return (-1);
         }
         return x;
     }
 
     public void initHardware() {
-        driveTrain = new DriveTrain(this);
-        rIntake = hardwareMap.dcMotor.get("rIntake");
-        lIntake = hardwareMap.dcMotor.get("lIntake");
-        lSlide = hardwareMap.dcMotor.get("lSlide");
-        rSlide = hardwareMap.dcMotor.get("rSlide");
-        rightSide = hardwareMap.dcMotor.get("rightSide");
-        leftSide = hardwareMap.dcMotor.get("leftSide");
 
-        leftSide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rightSide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rIntake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        lIntake.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        lSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        leftSide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightSide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rIntake.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        lIntake.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rSlide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        lSlide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        leftSide.setDirection(DcMotorSimple.Direction.FORWARD);
-        rightSide.setDirection(DcMotorSimple.Direction.REVERSE);
-        rIntake.setDirection(DcMotorSimple.Direction.REVERSE);
-        lIntake.setDirection(DcMotorSimple.Direction.FORWARD);
-        lSlide.setDirection(DcMotorSimple.Direction.REVERSE);
-        rSlide.setDirection(DcMotorSimple.Direction.FORWARD);
-        adaImu = hardwareMap.get(BNO055IMU.class, "IMU");
-        imu = new IMU(adaImu);
-
+      lWheel = hardwareMap.dcMotor.get("leftSide");
+      rWheel = hardwareMap.dcMotor.get("rightSide");
 
     }
 
