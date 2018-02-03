5814e8637e8d772bf39185dbd4ffe4a6f7560830
pranav <garg.pranav@gmail.com>
2018-02-03 03:09:21 -0800

    differential intake control

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tasks/IntakeTask.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tasks/IntakeTask.java
@@ -22,19 +22,28 @@ public class IntakeTask extends TaskThread {
     public void run() {
         timer.reset();
         while (opMode.opModeIsActive() && running) {
-            if (opMode.gamepad1.dpad_up || opMode.gamepad2.dpad_up) {
-                setRollerPower(-1);
-            } else if (opMode.gamepad1.dpad_down || opMode.gamepad2.dpad_down) {
+          /*  if (opMode.gamepad1.dpad_up || opMode.gamepad2.b) {
                 setRollerPower(1);
+            } else if (opMode.gamepad1.dpad_down || opMode.gamepad2.x) {
+                setRollerPower(-1);
             } else{
                 setRollerPower(0);
-            }
+            }*/
+            tankControl();
+
+
         }
     }
+//
 
     public void setRollerPower(double power) {
         rIntake.setPower(power);
         lIntake.setPower(power);
+    }
+    public void tankControl(){
+        lIntake.setPower(opMode.gamepad2.left_stick_y);
+        rIntake.setPower(opMode.gamepad2.right_stick_y);
+
     }
     @Override
     public void initialize() {
